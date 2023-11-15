a23ca0fcc327346ce2b1aa3f044182e3780357e4
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-11-15 14:43:21 +0300

    Adding predict grade problem cpp file

---
+++ conditionalStatments/predictGrade.cpp
@@ -0,0 +1,28 @@
+// Predict the Grade
+// Write a program  that reads the marks obtained by a student in 5 subjects and prints the grade of the student based on marks.
+
+// (Note: take the average of the marks obtained by the student to determine the grade)
+
+// if marks are less than 60,  then Grade is D
+
+// if marks are greater than or equal to 60 and less than 70,  then Grade is C
+
+// if marks are greater than or equal to 70 and less than 80,  then Grade is B
+
+// if marks are greater than or equal to 80 and less than 90,  then Grade is A
+
+// if marks are greater than or equal to 90, then Grade is A+
+
+// Input
+
+// 80.5
+
+// Output
+// A
+
+// Input
+
+// 50
+
+// Output
+// D
