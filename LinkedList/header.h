81081a9d3ceb5f2576f63877b4da4521df47a279
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-01 00:17:48 +0300

    implementing adding at the begining of the list functionality

--- LinkedList/header.h
+++ LinkedList/header.h
@@ -1,17 +1,48 @@
 #pragma once
 
-class Node
+class LinkedList;
+
+class Node // holds data value and a pointer to the next node element
 {
     int data;
     Node *next;
 
 public:
-    Node(int d) : data(d), next(nullptr){};
+    Node(int d) : data(d), next(NULL){}; // constructor initialization list
 
     int getData();
+    friend LinkedList;
 };
 
 int Node::getData()
 {
     return this->data;
+}
+
+class LinkedList
+{
+    Node *head;
+    Node *tail;
+
+public:
+    LinkedList() : head(NULL), tail(NULL){};
+
+    void push_front(int value){};
+};
+
+void LinkedList::push_front(int value)
+{
+    Node *n = new Node(value);
+    if (this->head == NULL)
+    {
+        // If the list is empty, set both head and tail to the new node
+        this->head = n;
+        this->tail = n;
+    }
+    else
+    {
+        // If the list is not empty, update the pointers to insert at the front
+        n->next = this->head;
+        this->head = n;
+    }
 }
