b8392d4246846607f54a696a9289d319e917ace3
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-03 16:27:25 +0300

    Add stack folder

---
+++ _Stacks/stack.h
@@ -0,0 +1,5 @@
+// Stack is a container in which we can push, pop and see what is present in the top
+
+// Push O(1) add element to the top
+// Pop O(1) remove element from the top
+// Top O(1) show what is present on the top
