39a1b2c820133b853984d6a7265413cde4571f27
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-03 16:54:00 +0300

    Implementation of stack DS using LinkedList

--- _Stacks/stack.h
+++ _Stacks/stack.h
@@ -3,3 +3,57 @@
 // Push O(1) add element to the top
 // Pop O(1) remove element from the top
 // Top O(1) show what is present on the top
+
+// Implementation : can be build using arrays, vectors or linkedList
+
+// Using LinkedList
+template <typename T>
+class Stack; // forward declaration
+
+template <typename T>
+class Node
+{
+public:
+    T data;
+    Node<T> *next;
+
+    Node(T d) : data(d){};
+};
+
+template <typename T>
+class Stack
+{
+    Node<T> *head;
+
+public:
+    // constructor:
+    Stack() : head(NULL){};
+
+    // push:
+    void push(T data)
+    {
+        Node<T> *n = new Node<T>(data);
+        n->next = head;
+        head = n;
+    }
+
+    bool empty()
+    {
+        return head == NULL;
+    }
+
+    T top()
+    {
+        return this->head->data;
+    }
+
+    void pop()
+    {
+        if (head != NULL)
+        {
+            Node<T> *temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
