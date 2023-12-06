242d72ec30433d3c579e4d23817db5fa0a99f9b3
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-06 16:56:33 +0300

    Fix Bugs in Header File

--- _Queue/header.h
+++ _Queue/header.h
@@ -1,6 +1,10 @@
 // A queue is a data structure that follows the First In, First Out (FIFO) principle. In a queue, elements are added at the rear (enqueue) and removed from the front (dequeue). The element that has been in the queue the longest is the first to be removed.
 
 // Implementation of a Queue :
+
+template <typename T> // Forward Declaration
+class Queue;
+
 template <typename T>
 class Node
 {
@@ -9,27 +13,28 @@ class Node
 
 public:
     Node(T d) : data(d), next(NULL){};
+    friend class Queue<T>;
 };
 
 template <typename T>
 class Queue
 {
     Node<T> *head;
-    Node<T> *rare;
+    Node<T> *rear;
 
 public:
-    void enqueue(type value)
+    void enqueue(T value)
     {
         Node<T> *n = new Node(value);
 
         if (head == NULL)
         {
-            this->head = this->rare = n;
+            this->head = this->rear = n;
         }
         else
         {
-            this->rare->next = n;
-            this->rare = n;
+            this->rear->next = n;
+            this->rear = n;
         }
     }
 
@@ -42,10 +47,25 @@ public:
     {
         if (this->empty())
         {
-            cout << "Queue is empty....";
-            return;
+            std::cout << "Queue is empty....";
+            return T();
         }
 
         return this->head->data;
+    }
+
+    void dequeue()
+    {
+        if (this->empty())
+        {
+            std::cout << "Queue is empty....";
+        }
+        else
+        {
+            Node<T> *temp = this->head;
+            this->head = this->head->next;
+            temp->next = NULL;
+            delete temp;
+        }
     }
 };
