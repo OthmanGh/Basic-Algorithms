351317473b2fbe55b28ecc50c995dee60e23adf3
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-06 16:58:14 +0300

    Add testing data

--- _Queue/main.cpp
+++ _Queue/main.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 int main()
 {
+    // Testing with integers
+    Queue<int> intQueue;
+    intQueue.enqueue(10);
+    intQueue.enqueue(20);
+    intQueue.enqueue(30);
+
+    cout << "Front: " << intQueue.front() << endl;
+
+    intQueue.dequeue();
+    cout << "Front after dequeue: " << intQueue.front() << endl;
+
+    // Testing with strings
+    Queue<string> stringQueue;
+    stringQueue.enqueue("apple");
+    stringQueue.enqueue("banana");
+    stringQueue.enqueue("cherry");
+
+    cout << "Front: " << stringQueue.front() << endl;
+
+    stringQueue.dequeue();
+    cout << "Front after dequeue: " << stringQueue.front() << endl;
 
     return 0;
 }
