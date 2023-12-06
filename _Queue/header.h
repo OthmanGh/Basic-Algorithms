e2b4f46034af3de76f17a262da5889665f4efe45
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-06 16:34:16 +0300

    fix node class implementation & add enqueue method

--- _Queue/header.h
+++ _Queue/header.h
@@ -1,13 +1,35 @@
 // A queue is a data structure that follows the First In, First Out (FIFO) principle. In a queue, elements are added at the rear (enqueue) and removed from the front (dequeue). The element that has been in the queue the longest is the first to be removed.
 
 // Implementation of a Queue :
-
 template <typename T>
 class Node
 {
     T data;
-    Node<T> *head;
+    Node<T> *next;
 
 public:
-    Node(T d) : data(d), head(NULL);
+    Node(T d) : data(d), next(NULL){};
+};
+
+template <typename type>
+class Queue
+{
+    Node<type> *head;
+    Node<type> *rare;
+
+public:
+    void Enqueue(type value)
+    {
+        Node<type> *n = new Node(value);
+
+        if (head == NULL)
+        {
+            this->head = this->rare = n;
+        }
+        else
+        {
+            this->rare->next = n;
+            this->rare = n;
+        }
+    }
 };
