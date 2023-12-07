7107e6c552c1bcc9e94fe3d7acc88453696cd33f
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-07 14:06:39 +0300

    creating node class for queue elements

---
+++ _Queue/queue.h
@@ -0,0 +1,15 @@
+// A queue is a data structure that follows the First In, First Out (FIFO) principle. In a queue, elements are added at the rear (enqueue) and removed from the front (dequeue). The element that has been in the queue the longest is the first to be removed.
+
+template <typename T>
+class Queue;
+
+template <typename T>
+class Node
+{
+    T data;
+    Node *head;
+
+public:
+    Node(T d) : data(d), head(nullptr){};
+    friend class Queue;
+};
