ca324d88cacdc0ff5aceddfdb8fc032f02884a52
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-08 23:06:49 +0300

    Add more test data to see how queue ds will behave with different data types

--- _Queue/main.cpp
+++ _Queue/main.cpp
@@ -4,21 +4,60 @@
 
 using namespace std;
 
+#include <iostream>
+#include <string>
+
 int main()
 {
-    Queue<string> students_queue;
+    // Create a queue of integers
+    Queue<int> intQueue;
 
-    students_queue.enqueue("Jack");
-    students_queue.enqueue("David");
-    students_queue.enqueue("Chris");
-    students_queue.enqueue("Vincent");
+    // Test enqueue with integers
+    intQueue.enqueue(10);
+    intQueue.enqueue(20);
+    intQueue.enqueue(30);
 
-    cout << "Students in Queue : ";
-    while (!students_queue.empty())
+    // Test front and dequeue with integers
+    std::cout << "Front element (int): " << intQueue.front() << std::endl;
+    intQueue.dequeue();
+
+    // Test empty with integers
+    std::cout << "Is the int queue empty? " << (intQueue.empty() ? "Yes" : "No") << std::endl;
+
+    // Test dequeue until empty with integers
+    while (!intQueue.empty())
     {
-        cout << students_queue.front() << " -> ";
-        students_queue.dequeue();
+        std::cout << "Front element (int): " << intQueue.front() << std::endl;
+        intQueue.dequeue();
     }
+
+    // Try to dequeue from an empty int queue
+    intQueue.dequeue();
+
+    // Create a queue of strings
+    Queue<std::string> stringQueue;
+
+    // Test enqueue with strings
+    stringQueue.enqueue("Hello");
+    stringQueue.enqueue("World");
+    stringQueue.enqueue("!");
+
+    // Test front and dequeue with strings
+    std::cout << "Front element (string): " << stringQueue.front() << std::endl;
+    stringQueue.dequeue();
+
+    // Test empty with strings
+    std::cout << "Is the string queue empty? " << (stringQueue.empty() ? "Yes" : "No") << std::endl;
+
+    // Test dequeue until empty with strings
+    while (!stringQueue.empty())
+    {
+        std::cout << "Front element (string): " << stringQueue.front() << std::endl;
+        stringQueue.dequeue();
+    }
+
+    // Try to dequeue from an empty string queue
+    stringQueue.dequeue();
 
     return 0;
 }
