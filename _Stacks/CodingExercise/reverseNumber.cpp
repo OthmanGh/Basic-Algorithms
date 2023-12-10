0c4fbb3ba46af30934b3b5976b0a3f3eb13cffd5
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-10 14:22:08 +0300

    add coding exercise 1

---
+++ _Stacks/CodingExercise/reverseNumber.cpp
@@ -0,0 +1,35 @@
+// Reverse a number using stack
+// Given a number , write a program to reverse this number using stack.
+
+// Input Format
+
+// In the function an integer is passed
+
+// Output Format
+
+// Return an integer
+
+// Sample Input
+
+// 456
+// Sample Output
+
+// 654
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int reverse(int n)
+{
+}
+
+int main()
+{
+    int number{456};
+
+    int reversed_num = reverse(number);
+
+    std::cout << "Reversed Number : " << reversed_num;
+}
