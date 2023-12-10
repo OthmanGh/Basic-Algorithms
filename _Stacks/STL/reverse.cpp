561f5ced63eddd64fc70aabae24e41ccdada4c7c
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-10 13:32:32 +0300

    add reverse stack problem solution

--- _Stacks/STL/reverse.cpp
+++ _Stacks/STL/reverse.cpp
@@ -2,8 +2,36 @@
 #include <stack>
 
 using namespace std;
+
+void insertAtBottom(stack<int> &s, int data)
+{
+    if (s.empty())
+    {
+        s.push(data);
+        return;
+    }
+
+    int temp = s.top();
+    s.pop();
+
+    insertAtBottom(s, data);
+    s.push(temp);
+}
+
 // Challenge: Reverse the Stack
-void reverse(stack<int> &s) {}
+void reverse(stack<int> &s)
+{
+
+    if (s.empty())
+    {
+        return;
+    }
+
+    int t = s.top();
+    s.pop();
+    reverse(s);           // reverse smaller stack
+    insertAtBottom(s, t); // insert top at the bottom of the stack
+}
 
 int main()
 {
@@ -12,6 +40,8 @@ int main()
     s.push(2);
     s.push(3);
     s.push(4);
+
+    reverse(s);
 
     while (!s.empty())
     {
